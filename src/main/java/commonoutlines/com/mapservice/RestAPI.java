1d3de2f40b23af288fe32944541e01adb9527471
CommonOutlines <CommonOutlines@gmail.com>
2023-09-14 00:11:16 -0700

    initial springboot project

---
+++ src/main/java/commonoutlines/com/mapservice/RestAPI.java
@@ -0,0 +1,13 @@
+package commonoutlines.com.mapservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestAPI {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
