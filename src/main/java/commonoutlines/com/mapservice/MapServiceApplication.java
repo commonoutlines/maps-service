e48cc18a1560945c0bbc77e952d1b0b9d859b2dd
CommonOutlines <CommonOutlines@gmail.com>
2023-09-16 01:47:29 -0700

    More PostGres

--- src/main/java/commonoutlines/com/mapservice/MapServiceApplication.java
+++ src/main/java/commonoutlines/com/mapservice/MapServiceApplication.java
@@ -2,6 +2,13 @@ package commonoutlines.com.mapservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@EnableJpaRepositories("commonoutlines.com.mapservice.*")
+@EntityScan("commonoutlines.com.mapservice.*")
 
 @SpringBootApplication
 public class MapServiceApplication {
