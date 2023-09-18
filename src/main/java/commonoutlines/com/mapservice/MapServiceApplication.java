89c0eb1c9068d17c876adddbfdc1add0d23bcda7
CommonOutlines <CommonOutlines@gmail.com>
2023-09-18 14:56:37 -0700

    removing jpa

--- src/main/java/commonoutlines/com/mapservice/MapServiceApplication.java
+++ src/main/java/commonoutlines/com/mapservice/MapServiceApplication.java
@@ -3,9 +3,6 @@ package commonoutlines.com.mapservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.boot.CommandLineRunner;
 
